f3c0c17101a104ef0fe79e7eebae5bd98a28b7d4
Nicolas-adeito <citdebugger@gmail.com>
2026-07-15 14:22:27 +0100

    Add files via upload

---
+++ view/template/footer.php
@@ -0,0 +1,10 @@
+</main>
+<footer>
+  <div class="text-white m-0">
+    <div class="container">
+      <p class="lead mb-0">Tous droits réservés</p>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
